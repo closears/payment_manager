bd081d12063f1ea2746aa8df730c9464b74740e2
unknown <zhaozengguang@163.com>
2015-06-03 15:57:29 +0800

    add db init code in run.py
    dev
    
    Signed-off-by: zhaozengguang <zhaozengguang@163.com>

--- run.py
+++ run.py
@@ -1,6 +1,27 @@
 # coding=utf-8
-from controller import app
+from models import User, Role
+from controller import app, db
+
+def init():
+    db.create_all()
+	try:
+        user = User.query.filter(
+            User.name == 'admin').one()
+    except:        
+        user = User(name='admin', password='admin')
+        db.session.add(user)
+        db.session.commit()
+    try:
+        role = Role.query.filter(
+            Role.name == 'admin').one()
+    except:
+        role = Role(name='admin')
+        db.session.add(role)
+        db.session.commit()
+    user.roles.append(role)
+    db.session.commit()
 
 if __name__ == '__main__':
     app.config.from_pyfile('config.cfg', silent=True)
+    init()
     app.run()
